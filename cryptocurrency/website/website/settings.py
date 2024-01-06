41604635e3a0dd4635afe2734098d76e9409a881
kwextech <kwextech@gmail.com>
2024-01-06 14:44:15 +0100

    last update

--- cryptocurrency/website/website/settings.py
+++ cryptocurrency/website/website/settings.py
@@ -161,8 +161,8 @@ DEFAULT_FROM_EMAIL ='support@zenithport.com'
 
 
 
-#AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME = 'projectbuckect'
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'projectbuckect'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
